7d8404ad8d2e3c14e31d698f1a516875a8168c43
anishagrawal <anishagrawal2003@gmail.com>
2024-05-21 02:54:52 -0400

    updated to all required routes to snpe

--- src/resy_api_gateway.rs
+++ src/resy_api_gateway.rs
@@ -2,7 +2,7 @@ use std::error::Error;
 use prettytable::{row, Table};
 use prettytable::cell::Cell;
 use prettytable::row::Row;
-use reqwest::Client;
+use reqwest::{Client, Response};
 use reqwest::header::{AUTHORIZATION, HeaderMap, HeaderValue};
 use serde_json::{json, Value};
 
@@ -47,70 +47,58 @@ impl ResyAPIGateway {
         }
     }
 
-    pub async fn get_user(&self) -> Result<Value, Box<dyn Error>> {
-        let url = format!("{}/2/user", RESY_API_BASE_URL);
+    async fn process_response(response: Response) -> Result<Value, Box<dyn Error>> {
+        if response.status().is_success() {
+            let json = response.json().await?;
+            Ok(json)
+        } else {
+            Err(Box::new(ResyAPIError {
+                message: format!("API request failed: {}", response.status())
+            }))
+        }
+    }
 
+    fn setup_headers(&self) -> HeaderMap {
         let mut headers = HeaderMap::new();
         headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("ResyAPI api_key=\"{}\"", self.api_key)).unwrap());
         headers.insert("x-resy-auth-token", HeaderValue::from_str(&self.auth_token).unwrap());
+        headers
+    }
+
+    pub async fn get_user(&self) -> Result<Value, Box<dyn Error>> {
+        let url = format!("{}/2/user", RESY_API_BASE_URL);
+        let headers = self.setup_headers();
 
         let res = self.client.get(url)
             .headers(headers)
             .send()
             .await?;
 
-        if res.status().is_success() {
-            let response = res.json().await?;
-            Ok(response)
-        } else {
-            Err(Box::new(ResyAPIError {
-                message: format!("Failed to fetch user: {}", res.status())
-            }))
-        }
+        Self::process_response(res).await
     }
 
     pub async fn get_venue(&self, venue_slug: &str) -> Result<Value, Box<dyn Error>> {
         let url = format!("{}/3/venue?url_slug={}&location=new-york-ny", RESY_API_BASE_URL, venue_slug);
-
-        let mut headers = HeaderMap::new();
-        headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("ResyAPI api_key=\"{}\"", self.api_key)).unwrap());
-        headers.insert("x-resy-auth-token", HeaderValue::from_str(&self.auth_token).unwrap());
+        let headers = self.setup_headers();
 
         let res = self.client.get(url)
             .headers(headers)
             .send()
             .await?;
 
-        if res.status().is_success() {
-            let response = res.json().await?;
-            Ok(response)
-        } else {
-            Err(Box::new(ResyAPIError {
-                message: format!("Failed to fetch venue: {}", res.status())
-            }))
-        }
+        Self::process_response(res).await
     }
 
     pub async fn find_reservation(&self, venue_id: &str, day: &str, party_size: u8) -> Result<Value, Box<dyn Error>> {
         let url = format!("{}/4/find?lat=0&long=0&day={}&party_size={}&venue_id={}", RESY_API_BASE_URL, day, party_size, venue_id);
-
-        let mut headers = HeaderMap::new();
-        headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("ResyAPI api_key=\"{}\"", self.api_key))?);
-        headers.insert("x-resy-auth-token", HeaderValue::from_str(&self.auth_token)?);
+        let headers = self.setup_headers();
 
         let res = self.client.get(url)
             .headers(headers)
             .send()
             .await?;
 
-        if res.status().is_success() {
-            let response = res.json().await?;
-            Ok(response)
-        } else {
-            Err(Box::new(ResyAPIError {
-                message: format!("Failed to find reservation slots: {}", res.status())
-            }))
-        }
+        Self::process_response(res).await
     }
 
     pub async fn get_reservation_details(
@@ -119,12 +107,9 @@ impl ResyAPIGateway {
         config_id: &str,
         party_size: u8,
         day: &str,
-    ) -> Result<String, Box<dyn Error>> {
+    ) -> Result<Value, Box<dyn Error>> {
         let url = format!("{}/3/details", RESY_API_BASE_URL);
-
-        let mut headers = HeaderMap::new();
-        headers.insert(AUTHORIZATION, HeaderValue::from_str(&format!("ResyAPI api_key=\"{}\"", self.api_key))?);
-        headers.insert("x-resy-auth-token", HeaderValue::from_str(&self.auth_token)?);
+        let headers = self.setup_headers();
 
         let data = json!({
             "commit": commit,
@@ -139,13 +124,24 @@ impl ResyAPIGateway {
             .send()
             .await?;
 
-        if res.status().is_success() {
-            let response = res.json().await?;
-            Ok(response)
-        } else {
-            Err(Box::new(ResyAPIError {
-                message: format!("Failed to find reservation details: {}", res.status())
-            }))
-        }
+        Self::process_response(res).await
+    }
+
+    pub async fn book_reservation(&self, book_token: &str, payment_id: i32) -> Result<Value, Box<dyn Error>> {
+        let url = format!("{}/3/book", RESY_API_BASE_URL);
+        let headers = self.setup_headers();
+
+        let body = format!(
+            "book_token={}&struct_payment_method={{\"id\":{}}}",
+            urlencoding::encode(book_token), payment_id
+        );
+
+        let res = self.client.post(&url)
+            .headers(headers)
+            .body(body)
+            .send()
+            .await?;
+
+        Self::process_response(res).await
     }
 }
